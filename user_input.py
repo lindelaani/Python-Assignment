c22820f3b8ea635e870e9577c1d9981cfc453d9a
lindelani xhanti <onebiz.innovation@gmail.com>
2024-08-17 22:00:37 +0200

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,11 @@
+def display_info(name, age, location):
+    print(f"• Hello, {name}")
+    print(f"•{age}")
+    print(f"•{location}")
+
+# Example usage
+name = "what is your name?"
+age = " How old are you ?"
+location = " Where do you stay?"
+display_info(name, age, location)
+ 
